3d20193eaccebc90ca2c4ccf2fc8579cdcd6d920
CityHunter2024 <wangdc1111@hotmail.com>
2024-07-12 21:00:35 +0800

    refactored code to process multiple tcam files once

--- main.cpp
+++ main.cpp
@@ -22,75 +22,78 @@ const std::unordered_map<std::string, std::string> c2_c4_map = {
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        return -1;
-    }
-
-    if (!std::filesystem::exists(argv[1])) {
-        std::cout << "source file dose not exists." << std::endl;
-        return -1;
-    }
-
-    if (!std::filesystem::is_regular_file(argv[1])) {
-        std::cout << "source must be a regular file." << std::endl;
-        return -1;
-    }
-
-    if (std::filesystem::directory_entry(argv[1]).path().extension() != ".tcam") {
-        std::cout << "source file must be a '.tcam' file." << std::endl;
-        return -1;
-    }
-
-    auto fname = std::string(argv[1]);
-    std::ifstream ifstr(fname);
-    if (!ifstr.is_open()) {
-        std::cout << "cannot open source file: " << fname << std::endl;
+    if (argc < 2) {
         return -1;
     }
 
     std::vector<std::vector<std::string>> sram_chip_vec{20};
-    for (auto &sram_entry_vec : sram_chip_vec) {
-        sram_entry_vec.emplace_back("");
-        sram_entry_vec.emplace_back("");
-        sram_entry_vec.emplace_back("");
-        sram_entry_vec.emplace_back("");
-    }
 
-    std::string line;
-    std::size_t sz = 0;
-    while (getline(ifstr, line)) {
-        const std::regex r(R"([01x]{40})");
-        if (!regex_match(line, r)) {
-            std::cout << "tcam entry shall only include 40 collums of [01x]." << std::endl;
+    for (auto j = 1; j < argc; ++j) {
+        if (!std::filesystem::exists(argv[j])) {
+            std::cout << "source file dose not exists." << std::endl;
             return -1;
         }
 
-        std::vector<std::string> tcam_entry_vec;
-        for (auto i = 0UL; i < line.size(); ++i, ++i) {
-            tcam_entry_vec.emplace_back(line.substr(i, 2));
+        if (!std::filesystem::is_regular_file(argv[j])) {
+            std::cout << "source must be a regular file." << std::endl;
+            return -1;
         }
 
-        std::reverse(tcam_entry_vec.begin(), tcam_entry_vec.end());
-
-        auto i = 0UL;
-        for (const auto &c2 : tcam_entry_vec) {
-            auto c4 = c2_c4_map.at(c2);
-            sram_chip_vec[i][0].insert(0, 1, c4[0]);
-            sram_chip_vec[i][1].insert(0, 1, c4[1]);
-            sram_chip_vec[i][2].insert(0, 1, c4[2]);
-            sram_chip_vec[i][3].insert(0, 1, c4[3]);
-            ++i;
+        if (std::filesystem::directory_entry(argv[j]).path().extension() != ".tcam") {
+            std::cout << "source file must be a '.tcam' file." << std::endl;
+            return -1;
         }
 
-        ++sz;
+        auto fname = std::string(argv[j]);
+        std::ifstream ifstr(fname);
+        if (!ifstr.is_open()) {
+            std::cout << "cannot open source file: " << fname << std::endl;
+            return -1;
+        }
+
+        for (auto &sram_entry_vec : sram_chip_vec) {
+            sram_entry_vec.emplace_back("");
+            sram_entry_vec.emplace_back("");
+            sram_entry_vec.emplace_back("");
+            sram_entry_vec.emplace_back("");
+        }
+
+        std::string line;
+        std::size_t sz = 0;
+        while (getline(ifstr, line)) {
+            const std::regex r(R"([01x]{40})");
+            if (!regex_match(line, r)) {
+                std::cout << "tcam entry shall only include 40 collums of [01x]." << std::endl;
+                return -1;
+            }
+
+            std::vector<std::string> tcam_entry_vec;
+            for (auto i = 0UL; i < line.size(); ++i, ++i) {
+                tcam_entry_vec.emplace_back(line.substr(i, 2));
+            }
+
+            std::reverse(tcam_entry_vec.begin(), tcam_entry_vec.end());
+
+            auto i = 0UL;
+            for (const auto &c2 : tcam_entry_vec) {
+                auto c4 = c2_c4_map.at(c2);
+                sram_chip_vec[i][(j-1)*4+0].insert(0, 1, c4[0]);
+                sram_chip_vec[i][(j-1)*4+1].insert(0, 1, c4[1]);
+                sram_chip_vec[i][(j-1)*4+2].insert(0, 1, c4[2]);
+                sram_chip_vec[i][(j-1)*4+3].insert(0, 1, c4[3]);
+                ++i;
+            }
+
+            ++sz;
+        }
+
+        if (sz != 32) {
+            std::cout << "a tcam table must be exactly 32 entries." << std::endl;
+            return -1;
+        }
     }
 
-    if (sz != 32) {
-        std::cout << "a tcam table must be exactly 32 entries." << std::endl;
-        return -1;
-    }
-
-    auto dst_fname = std::string(std::filesystem::directory_entry(fname).path().stem());
+    auto dst_fname = std::string(std::filesystem::directory_entry(std::string(argv[1])).path().stem());
     std::ofstream ofstr;
 
     std::vector<std::uint32_t> u32_vec;
